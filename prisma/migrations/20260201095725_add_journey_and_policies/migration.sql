5010e93d5a09b6996ccd962db6ae5d878d6bae70
Affan Khan <mohdaffankhan@users.noreply.github.com>
2026-02-02 23:11:42 +0530

    add new models

---
+++ prisma/migrations/20260201095725_add_journey_and_policies/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "PrivacyPolicy" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "effectiveDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PrivacyPolicy_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TermsOfService" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "effectiveDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TermsOfService_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Journey" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "coverImage" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Journey_pkey" PRIMARY KEY ("id")
+);
