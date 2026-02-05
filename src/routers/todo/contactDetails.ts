42e982b8fbc04d4077d05e333810d8d27665b06c
rajul-triadacademy <rajulthakur34@gmail.com>
2026-02-05 15:09:28 +0530

    update route contact details

--- src/routers/todo/contactDetails.ts
+++ src/routers/todo/contactDetails.ts
@@ -3,6 +3,6 @@ import { getContactDetails } from '../../controllers/todo/contactDetails.js';
 
 const contactDetailsRouter = express.Router();
 
-contactDetailsRouter.get('/:id?', getContactDetails);
+contactDetailsRouter.get('/:id', getContactDetails);
 
 export default contactDetailsRouter;
